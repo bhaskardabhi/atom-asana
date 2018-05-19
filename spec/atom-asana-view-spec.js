5c9ffd179e6927ee3ab5455e4ea7d06b4461b4d3
bhaskar <bhaskar@oviyum.com>
2018-05-19 12:26:52 +0530

    Added initial code

---
+++ spec/atom-asana-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAsanaView from '../lib/atom-asana-view';
+
+describe('AtomAsanaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
